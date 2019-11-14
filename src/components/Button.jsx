3a2b79c1bc92f0e703bd5c6cadf79f3e555e2b80
kostas <konstantinos.voulgaridis@aspisid.com>
2019-11-14 16:35:35 +0200

    Create Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../sass/Button.module.sass';
+
+const Button = ({ onClick }) => {
+    return (
+        <input
+            type='button'
+            className={styles.button}
+            value='SEARCH'
+            onClick={onClick}
+        />
+    );
+};
+
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired
+};
+
+export default Button;
